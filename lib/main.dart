2dbfbe4b87569c5ae5782cda12e015c9e6ff1fc8
Shawn Mathew <Shawnm-MAD>
2025-03-09 22:18:20 -0400

    Fixed indentations along with table_calendar dependencies in pubspec.yaml and errors in TableCalendar and variable _selectedDate

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
+
 void main() {
   runApp(AdoptionTravelApp());
 }
@@ -10,15 +11,10 @@ class Plan {
   DateTime date;
   bool isCompleted;
 
-  Plan({
-    required this.name,
-    required this.description,
-    required this.date,
-    this.isCompleted = false,
-  });
+  Plan({required this.name, required this.description, required this.date, this.isCompleted = false});
 }
 
- class AdoptionTravelApp extends StatelessWidget {
+class AdoptionTravelApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -38,9 +34,9 @@ class _PlanManagerScreenState extends State<PlanManagerScreen> {
   final List<Plan> _plans = [];
   DateTime _selectedDate = DateTime.now();
 
-  void _createPlan(String name, String description, DateTime date) {
+  void _createPlan(String name, String description) {
     setState(() {
-      _plans.add(Plan(name: name, description: description, date: date));
+      _plans.add(Plan(name: name, description: description, date: _selectedDate));
     });
   }
 
@@ -49,49 +45,43 @@ class _PlanManagerScreenState extends State<PlanManagerScreen> {
       _plans[index].isCompleted = !_plans[index].isCompleted;
     });
   }
+
   void _editPlan(int index) {
-  TextEditingController nameController = TextEditingController(text: _plans[index].name);
-  TextEditingController descriptionController = TextEditingController(text: _plans[index].description);
+    TextEditingController nameController = TextEditingController(text: _plans[index].name);
+    TextEditingController descriptionController = TextEditingController(text: _plans[index].description);
 
-  void _showCreatePlanModal(BuildContext context) {
-  TextEditingController nameController = TextEditingController();
-  TextEditingController descriptionController = TextEditingController();
-
-  showDialog(
-    context: context,
-    builder: (context) => AlertDialog(
-      title: Text('Create Plan'),
-      content: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          TextField(controller: nameController, decoration: InputDecoration(labelText: 'Name')),
-          TextField(controller: descriptionController, decoration: InputDecoration(labelText: 'Description')),
-          SizedBox(height: 10),
-          Text("Selected Date: ${_selectedDate.toLocal()}"),
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('Edit Plan'),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextField(controller: nameController, decoration: InputDecoration(labelText: 'Name')),
+            TextField(controller: descriptionController, decoration: InputDecoration(labelText: 'Description')),
+          ],
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
+              if (nameController.text.isNotEmpty) {
+                _editPlanInList(index, nameController.text, descriptionController.text);
+                Navigator.pop(context);
+              }
+            },
+            child: Text('Save'),
+          ),
         ],
       ),
-      actions: [
-        TextButton(
-          onPressed: () {
-            if (nameController.text.isNotEmpty) {
-              _createPlan(nameController.text, descriptionController.text);
-              Navigator.pop(context);
-            }
-          },
-          child: Text('Create'),
-        ),
-      ],
-    ),
-  );
-}
-
+    );
+  }
 
   void _editPlanInList(int index, String newName, String newDescription) {
-  setState(() {
-    _plans[index].name = newName;
-    _plans[index].description = newDescription;
-  });
-}
+    setState(() {
+      _plans[index].name = newName;
+      _plans[index].description = newDescription;
+    });
+  }
 
   void _deletePlan(int index) {
     setState(() {
@@ -99,51 +89,84 @@ class _PlanManagerScreenState extends State<PlanManagerScreen> {
     });
   }
 
-  @override
-Widget build(BuildContext context) {
-  List<Plan> _filteredPlans = _plans.where((plan) => isSameDay(plan.date, _selectedDate)).toList();
+  void _showCreatePlanModal(BuildContext context) {
+    TextEditingController nameController = TextEditingController();
+    TextEditingController descriptionController = TextEditingController();
 
-  return Scaffold(
-    appBar: AppBar(title: Text('Adoption & Travel Plans')),
-    body: Column(
-      children: [
-        // Calendar Widget
-        TableCalendar(
-          firstDay: DateTime.utc(2020, 1, 1),
-          lastDay: DateTime.utc(2030, 12, 31),
-          focusedDay: _selectedDate,
-          selectedDayPredicate: (day) => isSameDay(day, _selectedDate),
-          onDaySelected: (selectedDay, focusedDay) {
-            setState(() {
-              _selectedDate = selectedDay;
-            });
-          },
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('Create Plan'),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextField(controller: nameController, decoration: InputDecoration(labelText: 'Name')),
+            TextField(controller: descriptionController, decoration: InputDecoration(labelText: 'Description')),
+            SizedBox(height: 10),
+            Text("Selected Date: ${_selectedDate.toLocal()}"),
+          ],
         ),
-        Expanded(
-          child: ListView.builder(
-            itemCount: _filteredPlans.length,
-            itemBuilder: (context, index) {
-              final plan = _filteredPlans[index];
-              return Dismissible(
-                key: Key(plan.name),
-                onDismissed: (_) => _deletePlan(index),
-                background: Container(color: Colors.red),
-                child: ListTile(
-                  title: Text(plan.name),
-                  subtitle: Text(plan.description),
-                  tileColor: plan.isCompleted ? Colors.green[100] : Colors.red[100],
-                  onLongPress: () => _editPlan(index),
-                  onTap: () => _markCompleted(index),
-                ),
-              );
+        actions: [
+          TextButton(
+            onPressed: () {
+              if (nameController.text.isNotEmpty) {
+                _createPlan(nameController.text, descriptionController.text);
+                Navigator.pop(context);
+              }
+            },
+            child: Text('Create'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    List<Plan> _filteredPlans = _plans.where((plan) => isSameDay(plan.date, _selectedDate)).toList();
+
+    return Scaffold(
+      appBar: AppBar(title: Text('Adoption & Travel Plans')),
+      body: Column(
+        children: [
+          // Calendar Widget
+          TableCalendar(
+            firstDay: DateTime.utc(2020, 1, 1),
+            lastDay: DateTime.utc(2030, 12, 31),
+            focusedDay: _selectedDate,
+            selectedDayPredicate: (day) => isSameDay(day, _selectedDate),
+            onDaySelected: (selectedDay, focusedDay) {
+              setState(() {
+                _selectedDate = selectedDay;
+              });
             },
           ),
-        ),
-      ],
-    ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: () => _showCreatePlanModal(context),
-      child: Icon(Icons.add),
-    ),
-  );
+          Expanded(
+            child: ListView.builder(
+              itemCount: _filteredPlans.length,
+              itemBuilder: (context, index) {
+                final plan = _filteredPlans[index];
+                return Dismissible(
+                  key: Key(plan.hashCode.toString()), // Ensure unique key
+                  onDismissed: (_) => _deletePlan(index),
+                  background: Container(color: Colors.red),
+                  child: ListTile(
+                    title: Text(plan.name),
+                    subtitle: Text(plan.description),
+                    tileColor: plan.isCompleted ? Colors.green[100] : Colors.red[100],
+                    onLongPress: () => _editPlan(index),
+                    onTap: () => _markCompleted(index),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _showCreatePlanModal(context),
+        child: Icon(Icons.add),
+      ),
+    );
+  }
 }
